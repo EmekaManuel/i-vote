a3e844d89a82ed68c00cf48da7fec09786bd8733
Emeka Manuel <apple@Manuels-MacBook-Pro.local>
2023-04-06 00:19:13 +0100

    correction for the jwt auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        jwt.verify(token, process.env.SECRET, (error, decoded) => {
             if (error) {
                 next(Error ('Failed to authenticate Token'))
             }
