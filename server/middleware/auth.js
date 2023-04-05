262f99d54ee9576cd5b5dacf7224cc7e94726d8f
Emeka Manuel <apple@Manuels-MacBook-Pro.local>
2023-04-06 00:18:28 +0100

    added jwt auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,16 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
     if (req.headers.authorization) {
-        const token = req.headers.authorization
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.SECRET, (err, decoded) => {
+            if (error) {
+                next(Error ('Failed to authenticate Token'))
+            }
+            else {
+                req.decoded = decoded;
+                next()
+            }
+        })
     }
     else {
         next(Error('No Token Provided'))
