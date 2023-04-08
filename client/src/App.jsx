d14f3cf6c1a34c0e633fd0ffe759fbe006a58002
Emeka Manuel <apple@Manuels-MacBook-Pro.local>
2023-04-08 08:19:11 +0100

    set up the api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Home from "./components/Home";
+import api from "./services/api";
 
-const App = () => {
-  return (
-    <div>App</div>
+const App = () =>  {
+    useEffect(() => {
+        async function fetchData() {
+          const result = await api.call("post", "auth/login", {
+            username: "username",
+            password: "password",
+          });
+          console.log(result);
+        }
+        fetchData();
+      }, []);
+      
+
+  return(
+    <Home/>
+
   )
 }
 
-export default App
+export default App;
